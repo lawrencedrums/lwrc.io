884f60c9b8e491677a06e7c12d5b6eeb87ae8564
Lawrence Wong <51978449+lawrencedrums@users.noreply.github.com>
2020-10-30 09:46:42 +0800

    Created project model

---
+++ django/project_api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='images', blank=True)
+    url = models.URLField()
+    repo = models.URLField(blank=True)
+    publish_date = models.DateField()
