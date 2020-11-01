210d336543615d88e6f158f2d2d13ad18af5d916
Lawrence Wong <51978449+lawrencedrums@users.noreply.github.com>
2020-11-01 18:26:48 +0800

    Added some homepage styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,11 @@ import RecentProjects from './components/RecentProjects'
 function App() {
   return (
     <div className="App">
-      <div className="Homepage">
-        <h1>Lawrence Wong</h1>
-        <p>You are not my friend, you're my brother, my friend.</p>
+      <div className="homepage">
+        <div className="home-title">
+          <h1>Lawrence Wong</h1>
+          <p>You are not my friend, you're my brother, my friend.</p>
+        </div>
       </div>
       <RecentProjects />
     </div>
