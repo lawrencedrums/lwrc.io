7a3677b79290fa2d6b18cfbe1ace56a7f29a2fa3
Lawrence Wong <51978449+lawrencedrums@users.noreply.github.com>
2020-11-01 23:28:55 +0800

    Added homepage background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import RecentProjects from './components/RecentProjects'
+import RecentProjects from './components/RecentProjects';
+import videoSource from './components/media/video.mp4';
 
 function App() {
   return (
     <div className="App">
       <div className="homepage">
+        <video autoPlay muted loop className="video-background">
+          <source src={videoSource} type="video/mp4" />
+        </video>
         <div className="home-title">
           <h1>Lawrence Wong</h1>
           <p>You are not my friend, you're my brother, my friend.</p>
