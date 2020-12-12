8073b3acc1a2d430b7dbbe81b973ceaadf47aaf9
Lawrence Wong <51978449+lawrencedrums@users.noreply.github.com>
2020-12-12 16:05:02 +0800

    Fixed projects boarders

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './Homepage.css';
-import videoSource from './media/video.mp4';
 
 class Homepage extends React.Component {
   render() {
     return (
       <div className="homepage">
-        <video autoPlay muted loop className="video-background">
-          <source src={videoSource} type="video/mp4" />
-        </video>
         <div className="home-title">
           <h1>Lawrence Wong</h1>
           <div className="home-description">
