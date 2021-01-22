43524f033bdfcc8d529ad79504250d70bed76941
Lawrence Wong <lawrencedrums@gmail.com>
2021-01-22 16:22:57 +0800

    Fixed fetched API not rendering

--- frontend/src/components/RecentProjects.js
+++ frontend/src/components/RecentProjects.js
@@ -3,32 +3,29 @@ import React, { useState, useEffect } from 'react';
 import './RecentProjects.css';
 
 const RecentProjects = () => {
-  const [projectsData, setProjectsData] = useState([null]);
+  const [projects, setProjects] = useState([]);
+
+  const fetchData = async () => {
+    const { data } = await axios.get('http://0.0.0.0:8000/recentprojects/?format=json');
+    setProjects(data);
+    console.log(data);
+  }
 
   useEffect(() => {
-    axios.get('http://0.0.0.0:8000/recentprojects/?format=json')
-    .then((response) => {
-      setProjectsData(response.data);
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  });
+    fetchData();
+  }, []);
 
   return (
     <div className="cards-container">
       <h1>Latest Projects</h1>
-
-        { projectsData.map((projects) => {
-          // return (
-            <a href={projects.url} target="_blank">
-              <div className="project-cards" key={projects.id}>
-                <img className="card-image" alt={projects.description} src={projects.image} />
-                <div className="card-title">{projects.title}</div>
-              </div>
-            </a>
-          // );
-        })}
+        {projects.map((project) => (
+          <a href={project.url} target="_blank">
+            <div className="project-cards" key={project.id}>
+              <img className="card-image" alt={project.description} src={project.image} />
+              <div className="card-title">{project.title}</div>
+            </div>
+          </a>
+        ))}
     </div>
   )
 }
