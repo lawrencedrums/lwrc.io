329266d8bc6a2bccf8706c0e60d33e4ecd3f586f
Lawrence Wong <lawrencedrums@gmail.com>
2021-01-26 00:03:58 +0800

    Fetching API moved to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,32 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import RecentProjects from './components/RecentProjects';
 import Homepage from './components/Homepage';
 import Features from './components/Features';
 
-function App() {
+const App = () => {
+  const [projects, setProjects] = useState([]);
+  const [isLoading, setIsLoading] = useState([true])
+
+  const fetchData = async () => {
+    console.log(isLoading);
+    const { data } = await axios.get('http://0.0.0.0:8000/recentprojects/?format=json');
+    setProjects(data);
+    setIsLoading(false);
+    console.log(projects);
+    console.log(isLoading);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Homepage />
       <Features />
-      <RecentProjects />
+      <RecentProjects projects={projects}/>
     </div>
   );
 };
--- frontend/src/components/RecentProjects.js
+++ frontend/src/components/RecentProjects.js
@@ -2,23 +2,7 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './RecentProjects.css';
 
-const RecentProjects = () => {
-  const [projects, setProjects] = useState([]);
-  const [isLoading, setIsLoading] = useState([true])
-
-  const fetchData = async () => {
-    console.log(isLoading);
-    const { data } = await axios.get('http://0.0.0.0:8000/recentprojects/?format=json');
-    setProjects(data);
-    setIsLoading(false);
-    console.log(projects);
-    console.log(isLoading);
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+const RecentProjects = ({projects}) => {
   return (
     <div className="cards-container">
       <h1>Latest Projects</h1>
