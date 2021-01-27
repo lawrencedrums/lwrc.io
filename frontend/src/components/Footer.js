438854e579d8df1962537fe67b38a40e5e5ed941
Lawrence Wong <lawrencedrums@gmail.com>
2021-01-27 22:34:54 +0800

    Text links in socials now references dict

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -31,30 +31,18 @@ const Footer = () => {
         <h1>Born and bred in Hong Kong, now studying full-time in the UK.</h1>
         <p>
           Connect with me on{" "}
-          <a
-            href="https://www.linkedin.com/in/lawrence-wong-271157173/"
-            rel="noreferrer"
-            target="_blank"
-          >
+          <a href={socialNetworks[0].link} rel="noreferrer" target="_blank">
             LinkedIn
           </a>{" "}
           and{" "}
-          <a
-            href="https://www.instagram.com/okaylaw"
-            rel="noreferrer"
-            target="_blank"
-          >
+          <a href={socialNetworks[2].link} rel="noreferrer" target="_blank">
             Instagram
           </a>
           .
         </p>
         <p>
           Subscribe and leave a comment on my{" "}
-          <a
-            href="https://www.youtube.com/channel/UCMJyv6Z5PhGUkm-SJjZCp4g"
-            rel="noreferrer"
-            target="_blank"
-          >
+          <a href={socialNetworks[1].link} rel="noreferrer" target="_blank">
             YouTube
           </a>{" "}
           channel to really grab my attention.
