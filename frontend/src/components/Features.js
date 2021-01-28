98ba43abfd3a44ad2de20ce949198e35a308d1b5
Lawrence Wong <lawrencedrums@gmail.com>
2021-01-28 15:20:08 +0800

    Features overhauled

--- frontend/src/components/Features.js
+++ frontend/src/components/Features.js
@@ -1,24 +1,18 @@
 import React from "react";
 import "./Features.css";
-import drumsPNG from "./media/grid-band-bro.png";
-import recordPNG from "./media/grid-Recording-bro.png";
-import studioPNG from "./media/grid-Architect-bro.png";
 
 const cardItems = [
   {
-    image: drumsPNG,
     title: "Musicianship",
     description:
       "Whether it be Hip Hop, Rock, Metal, Pop, or Funk. Add some groove to your music.",
   },
   {
-    image: recordPNG,
     title: "Sound Engineering",
     description:
       "Need your music recorded, mixed, and mastered? Our producers have you covered.",
   },
   {
-    image: studioPNG,
     title: "Studio Design",
     description:
       "Recording studio acoustic treatment and design done with precision and care.",
@@ -30,13 +24,6 @@ const Features = () => {
     return (
       <div key={item.title}>
         <div className="card-item">
-          <div className="card-image-container">
-            <img
-              className="card-item-image"
-              src={item.image}
-              alt="Failed to load."
-            />
-          </div>
           <div className="card-item-title">{item.title}</div>
           <div className="card-item-description">{item.description}</div>
         </div>
@@ -46,10 +33,6 @@ const Features = () => {
 
   return (
     <div className="features-container">
-      <div className="features-quote">
-        "Possibly the best drummer in the band."
-        <p>- The guitarist</p>
-      </div>
       <div className="features-banner">
         <span>Real drums. Real good.</span>
         <div className="line-separator" />
