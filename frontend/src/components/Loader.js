21ebcca1d2860e1472079c12dbe095863089ca3c
Lawrence Wong <lawrencedrums@gmail.com>
2021-01-29 08:26:46 +0800

    Added spinning loader

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,9 +1,20 @@
 import React from "react";
+import loader from "./media/loader.gif";
 
 const Loader = () => {
   return (
-    <div className="cards-container">
+    <div
+      style={{ marginTop: "100px", marginBottom: "100px" }}
+      className="cards-container"
+    >
       <h1>LOADING MY PROJECTS...</h1>
+      <div className="loader-spinner">
+        <img
+          style={{ width: "64px", height: "64px" }}
+          src={loader}
+          alt="loader"
+        />
+      </div>
     </div>
   );
 };
