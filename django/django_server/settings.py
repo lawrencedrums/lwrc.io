3095c53121ea5e4f72aff074f2b2c3634f2d078a
Lawrence Wong <lawrencedrums@gmail.com>
2021-02-03 09:02:01 +0800

    All components optimized for mobile (exp projects)

--- django/django_server/settings.py
+++ django/django_server/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = get_secret('SECRET_KEY')
-SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
