c91b9235005b20d74e0d01cb99bddd577fb7642b
Lawrence Wong <lawrencedrums@gmail.com>
2021-02-07 19:29:55 +0800

    Implement styled-component in Intro

--- frontend/src/components/Intro.js
+++ frontend/src/components/Intro.js
@@ -1,34 +1,80 @@
 import React from "react";
-import "./Intro.css";
+import styled from "styled-components";
 import introImage from "./media/intro-image.jpg";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  background: #f0f0f0;
+`;
+
+const ContentWrapper = styled.div`
+  width: 80vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media only screen and (orientation: portrait) {
+    width: 95vw;
+    flex-direction: column;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  width: 400px;
+  height: 600px;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+
+  @media only screen and (orientation: portrait) {
+    width: 95%;
+    height: auto;
+  }
+`;
+
+const TextWrapper = styled.div`
+  width: 40vw;
+  text-align: left;
+  flex-direction: column;
+  padding-left: 50px;
+
+  h1 {
+    line-height: 1.2;
+    color: black;
+  }
+
+  @media only screen and (orientation: portrait) {
+    width: 95%;
+    margin: 5px;
+  }
+`;
 
 const Intro = () => {
   return (
-    <div className="intro-container">
-      <div className="intro-content-wrapper">
-        <div className="intro-image-container">
+    <Container>
+      <ContentWrapper>
+        <ImageWrapper>
           <img src={introImage} alt="intro" />
-        </div>
-        <div className="intro-text-wrapper">
-          <div className="intro-title">
-            <h1>
-              My passion is music. <br></br>My focus is engineering.
-            </h1>
-          </div>
-          <div className="intro-content">
-            <p>
-              I've played with and recorded many talented musicians in my 10+
-              years as a drummer and an audio engineer.
-            </p>
-            <p>
-              Always been obsessed and fascinated with tech, I will start my MSc
-              study in Computer Science at the University of Bath at the end of
-              2021.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+        </ImageWrapper>
+        <TextWrapper>
+          <h1>
+            My passion is music. <br></br>My focus is engineering.
+          </h1>
+          <p>
+            I've played with and recorded many talented musicians in my 10+
+            years as a drummer and an audio engineer.
+          </p>
+          <p>
+            Always been obsessed and fascinated with tech, I will start my MSc
+            study in Computer Science at the University of Bath at the end of
+            2021.
+          </p>
+        </TextWrapper>
+      </ContentWrapper>
+    </Container>
   );
 };
 
