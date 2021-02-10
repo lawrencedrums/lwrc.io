126cc6b26a2b56ba70bd495314358526c5ba3965
Lawrence Wong <lawrencedrums@gmail.com>
2021-02-10 21:41:49 +0800

    Implement styled-component in Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,64 @@
 import React from "react";
+import styled from "styled-components";
 import { Link } from "react-scroll";
 import "./Header.css";
 
+const Container = styled.div`
+  width: 100vw;
+  height: 8vh;
+  margin: 0px;
+  z-index: 1;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+
+  background-color: rgb(0, 0, 0, 0.5);
+  backdrop-filter: blur(6px);
+  border-bottom: 1px solid rgb(150, 150, 150, 0.5);
+
+  a {
+    color: white;
+  }
+`;
+
+const Content = styled.div`
+  width: 95%;
+  max-width: 900px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeaderTitle = styled.div`
+  font-size: 36px;
+  position: absolute;
+  left: 150px;
+  color: white;
+  text-decoration: none;
+`;
+
+const HeaderMenu = styled.div`
+  font-size: 18px;
+  position: absolute;
+  right: 150px;
+  color: white;
+
+  span {
+    padding-right: 40px;
+    cursor: pointer;
+  }
+`;
+
 const Header = () => {
   return (
-    <div className="header-container">
-      <div className="header-content-wrapper">
-        <a className="header-title" href="lwrc.io">
-          Lw.
-        </a>
-        <div className="header-links">
+    <Container>
+      <Content>
+        <HeaderTitle>
+          <a href="lwrc.io">Lw.</a>
+        </HeaderTitle>
+        <HeaderMenu>
           <span>
             <Link
               activeClass="active"
@@ -21,12 +70,12 @@ const Header = () => {
             </Link>
           </span>
           <span>
-            <Link to="intro-container" spy={true} smooth={true}>
+            <Link to="IntroContainer" spy={true} smooth={true}>
               About.
             </Link>
           </span>
           <span>
-            <Link to="projects-container" spy={true} smooth={true}>
+            <Link to="ProjectContainer" spy={true} smooth={true}>
               Projects.
             </Link>
           </span>
@@ -35,9 +84,9 @@ const Header = () => {
               Contact.
             </Link>
           </span>
-        </div>
-      </div>
-    </div>
+        </HeaderMenu>
+      </Content>
+    </Container>
   );
 };
 
