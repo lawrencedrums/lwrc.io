c021efe6cb02f71cc4b1048456d4a451df9afe7a
Lawrence Wong <lawrencedrums@gmail.com>
2021-02-12 13:11:15 +0800

    Setup store product view URL

--- django/django_server/urls.py
+++ django/django_server/urls.py
@@ -4,10 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
 from project_api.views import ProjectViewSet, RecentProjectViewSet
+from store.views import ProductViewSet
 
 router = routers.DefaultRouter()
 router.register('projects', ProjectViewSet)
 router.register('recentprojects', RecentProjectViewSet)
+router.register('storeproducts', ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
