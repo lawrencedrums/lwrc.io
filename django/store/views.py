243e990feeb216fafd49bd5c1746cd27444d9313
Lawrence Wong <lawrencedrums@gmail.com>
2021-02-12 13:07:25 +0800

    Created basic store products view

--- django/store/views.py
+++ django/store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects
+    serializer_class = ProductSerializer
