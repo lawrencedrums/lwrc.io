10cf80637ef698e15a95e6fc7fe9082104b1de46
Lawrence Wong <lawrencedrums@gmail.com>
2021-02-25 08:41:40 +0800

    Imprement the blame function

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -15,9 +15,25 @@ const Container = styled.div`
 `;
 
 const Loader = () => {
+  const [apology, setApology] = useState("(It might take a couple seconds...)");
+
+  const updateApology = () => {
+    setApology(
+      "(Please bear with me, I am only using the free tier on Heroku for this."
+    );
+  };
+
+  // Blame free tier Heroku after 10 seconds
+  useEffect(() => {
+    setTimeout(() => {
+      updateApology();
+    }, 10000);
+  }, []);
+
   return (
     <Container className="project-container">
       <h1>Loading Projects...</h1>
+      <p>{apology}</p>
       <img src={process.env.PUBLIC_URL + "/media/loader.gif"} alt="loader" />
     </Container>
   );
