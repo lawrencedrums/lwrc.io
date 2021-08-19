96c78dca41aec841e2f2bf8491d2665f93b5de3e
Lawrence Wong <lawrencedrums@gmail.com>
2021-08-19 16:25:44 +0800

    Added more content in about section

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -37,31 +37,34 @@ const About = () => {
     <Container>
       <ContentWrapper>
         <AboutText>
+          <h1>Hi, I'm Lawrence.</h1>
           <h1>
-            Hi, I'm Lawrence. A Drummer, Sound Engineer, and Software Engineer
-            based in Hong Kong.
+            Software Engineer, Sound Engineer, and Drummer based in Hong Kong.
           </h1>
           <AboutImage>(Photo coming soon)</AboutImage>
           <p>
             Always been obsessed with music and tech (and coffee), I can go on
-            and on about new trends and discoveries about them. Known as the
+            and on about new trends and discoveries on the topics. Known as the
             "tech guy" in my circles, I find enjoyment in solving technical
-            problems my friends have with their computers, and with my work in
-            digital music. One day, I had a simple, yet life-changing epiphany:
+            problems my friends have with their computers, as well as challenges
+            I face when working with digital music. For the past few years, I've
+            found myself being curious with the inner workings of digital audio
+            processing, Cryptocurrenies, and technologies that I use everyday.
+            One day, I had a simple, yet life-changing epiphany:
             <p>
               <cite>
                 "I should learn to <b>really</b> do it."
               </cite>
             </p>
-            <p>
-              So anyway, I started learning. I began with Python, finished CS50
-              in a month, wrote a sorting visualizer, got an offer for MSc
-              Computer Science, wrote my own personal website... I loved every
-              second of it. There's no doubt in my mind that I made the right
-              decision to learn programming. I'm eager to see where this
-              decision would take me in the future. Now, I'm a self-taught,
-              aspiring Software Engineer who can't stop learning and building.
-            </p>
+            So anyway, I started learning. I began with Python, finished CS50 in
+            a month, wrote a sorting visualizer, wrote my own personal website,
+            got an offer for MSc Computer Science... I loved every second of it.
+            Programming allows me to adopt the creativity and meticulousness I
+            developed as a music producer and sound engineer. There's no doubt
+            in my mind that I made the right decision to learn programming. I'm
+            eager to see where this decision would take me in the future. Now,
+            I'm a self-taught, aspiring Software Engineer who can't stop
+            learning and building.
           </p>
         </AboutText>
       </ContentWrapper>
